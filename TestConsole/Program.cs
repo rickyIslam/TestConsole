bf2e4238787e19efa92b65d657c736e5fa446e32
mdrickyislam.cse <mdrickyislam.cse@gmail.com>
2019-11-18 14:53:24 +0600

    initial comming to add files

---
+++ TestConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            featureOne();
+        }
+
+        static void featureOne ()
+        {
+            Console.WriteLine("This is just feature one");
+        }
+    }
+}
